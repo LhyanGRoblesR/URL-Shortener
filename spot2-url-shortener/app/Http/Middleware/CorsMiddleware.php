d25477a97ceb37c4ef584343a0d9c53ac78e1aad
lrobles <robleslhyan@gmail.com>
2024-04-03 16:19:16 -0500

    Fix

---
+++ spot2-url-shortener/app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://52.90.165.206') // Cambia esto al dominio de tu aplicación Next.js
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    }
+}
